151a691def9a65c559ee9295f8295df7de8a42a7
Arulselvan Madhavan <arulselvan1234@gmail.com>
2023-01-15 21:24:25 -0800

    Render on load

--- client/main.ml
+++ client/main.ml
@@ -27,32 +27,44 @@ end
 module Form = Bonsai_web_ui_form
 
 module Action = struct
-  type t = Spec of string
+  type t =
+    | Spec of string option
+    | Error of Error.t option
   (* JSOO - vegaEmbed *)
   (* | Image *)
   [@@deriving sexp_of]
 end
 
-let fetch_spec inject =
-  let open Effect.Let_syntax in
-  let%bind response = Effect.of_deferred_fun (fun p -> Async_js.Http.get p) "/spec" in
-  inject (Action.Spec (Or_error.ok_exn response))
-;;
-
-let handle_dd_change inject _ev _s = fetch_spec inject
-
-let handle_spec_change (state : M.t) _ev =
-  Option.iter state.spec ~f:(fun s ->
-    let json_spec =
-      Js.Unsafe.fun_call
-        (Js.Unsafe.js_expr "JSON.parse")
-        [| Js.Unsafe.inject (Js.string s) |]
-    in
+let handle_spec_change s =
+  let json_spec =
+    Js.Unsafe.fun_call
+      (Js.Unsafe.js_expr "JSON.parse")
+      [| Js.Unsafe.inject (Js.string s) |]
+  in
+  let _ =
     Js.Unsafe.fun_call
       (Js.Unsafe.js_expr "vegaEmbed")
-      [| Js.Unsafe.inject (Js.string "#viz"); json_spec |]);
-  Effect.Ignore
+      [| Js.Unsafe.inject (Js.string "#viz"); json_spec |]
+  in
+  ()
 ;;
+
+let fetch_spec inject s =
+  let open Effect.Let_syntax in
+  let%bind response =
+    Effect.of_deferred_fun
+      (fun p -> Async_js.Http.get ~arguments:[] p)
+      ("/recipe/" ^ s ^ ".vg.json")
+  in
+  if Core.Or_error.is_error response
+  then inject (Action.Error (Core.Result.error response))
+  else (
+    let spec = Core.Or_error.ok_exn response in
+    handle_spec_change spec;
+    inject (Action.Spec (Some spec)))
+;;
+
+let handle_dd_change inject _ev s = fetch_spec inject s
 
 let form_of_v (inject : (Action.t -> unit Effect.t) Value.t) : V.t Form.t Computation.t =
   Form.Typed.Variant.make
@@ -79,6 +91,22 @@ let form_of_v (inject : (Action.t -> unit Effect.t) Value.t) : V.t Form.t Comput
     end)
 ;;
 
+let handle_v_change inject = function
+  | V.Opt125m_output_range ->
+    fetch_spec
+      inject
+      (Base.String.lowercase (Sexp.to_string (V.sexp_of_t V.Opt125m_output_range)))
+  | _ -> Vdom.Effect.return ()
+;;
+
+let handle_form_change form_v (inject : (Action.t -> unit Effect.t) Value.t) =
+  let open! Bonsai.Let_syntax in
+  Form.Dynamic.on_change
+    (module V)
+    form_v
+    ~f:(Value.map inject ~f:(fun inject -> handle_v_change inject))
+;;
+
 let view_of_form : Vdom.Node.t Computation.t =
   let open! Bonsai.Let_syntax in
   let%sub state, inject =
@@ -89,21 +117,26 @@ let view_of_form : Vdom.Node.t Computation.t =
       ~default_model:M.default
       ~apply_action:(fun ~inject:_ ~schedule_event:_ model action ->
         match action with
-        | Spec s -> { model with spec = Some s })
+        | Spec s -> { model with spec = s }
+        | Error e -> { model with error = e })
   in
   let%sub form_v = form_of_v inject in
-  (* let%sub () = Form.Dynamic.on_change (module M) form_v ~f:handle_form_change in *)
+  let%sub () = handle_form_change form_v inject in
+  (* let%sub () = Form.Dynamic.on_change (module V) form_v ~f:(handle_v_change state) in *)
   let%arr form_v = form_v
   and state = state in
+  (* and inject = inject in *)
   let value = Form.value form_v in
+  (* let f = handle_v_change state in *)
   Vdom.Node.div
     [ Form.view_as_vdom form_v
+      (* ; Vdom.Node.select ~attr:(Vdom.Attr.many [Vdom.Attr.on_change (handle_dd_change inject)]) *)
     ; Vdom.Node.sexp_for_debugging ([%sexp_of: V.t Or_error.t] value)
     ; Vdom.Node.text (Sexp.to_string (M.sexp_of_t state))
-    ; Vdom.Node.button
-        ~key:"viz"
-        ~attr:(Vdom.Attr.many [ Vdom.Attr.on_click (handle_spec_change state) ])
-        [ Vdom.Node.text "Load Viz" ]
+      (* ; Vdom.Node.button *)
+      (*     ~key:"viz" *)
+      (*     ~attr:(Vdom.Attr.many [ Vdom.Attr.on_click (handle_spec_change state) ]) *)
+      (*     [ Vdom.Node.text "Load Viz" ] *)
     ]
 ;;
 
--- server/bin/main.ml
+++ server/bin/main.ml
@@ -31,30 +31,9 @@ let html =
 |}
 ;;
 
-let read_file filename = Stdlib.In_channel.with_open_text filename In_channel.input_all
-
-(* let body spec = *)
-(*   {|<body *)
-(*      <div id="app"></div> *)
-(*      <div id="vis"></div> *)
-(*      <script>vegaEmbed("#vis",|} *)
-(*   ^ spec *)
-(*   ^ {|);</script> *)
-(*    </body>|} *)
-(* ;; *)
-
-(* let head = *)
-(*   {| *)
-(*     <head> *)
-(* <script src="https://cdn.jsdelivr.net/npm/vega@5"></script> *)
-(* <script src="https://cdn.jsdelivr.net/npm/vega-lite@5"></script> *)
-(* <script src="https://cdn.jsdelivr.net/npm/vega-embed@6"></script> *)
-(*     </head>  *)
-(* |} *)
-(* ;; *)
-
-(* let gen_html spec = "<html>" ^ head ^ body spec ^ "</html>" *)
+(* let read_file filename = Stdlib.In_channel.with_open_text filename In_channel.input_all *)
 let csv_pattern = Base.String.Search_pattern.create ~case_sensitive:false "csv"
+let json_pattern = Base.String.Search_pattern.create ~case_sensitive:false "json"
 
 let server ~port =
   let callback _conn req _body =
@@ -68,16 +47,21 @@ let server ~port =
         ~status:`OK
         ~body:Embedded_files.main_dot_bc_dot_js
         ()
-    | "/spec" ->
-      let _ = Base.String.Search_pattern.matches csv_pattern uri in
-      respond_string
-        ~content_type:"application/json"
-        ~status:`OK
-        ~body:(read_file "recipe/opt125m_out_range.vg.json")
-        ()
+    (* | "/spec" -> *)
+    (*   let qargs = Uri.query (req |> Request.uri) in *)
+    (*   List.iter (fun (x, _xs) -> print_string @@ Printf.sprintf "%s\n" x) qargs; *)
+    (*   let _ = Base.String.Search_pattern.matches csv_pattern uri in *)
+    (*   respond_string *)
+    (*     ~content_type:"application/json" *)
+    (*     ~status:`OK *)
+    (*     ~body:(read_file "recipe/opt125m_out_range.vg.json") *)
+    (*     () *)
     | _ ->
+      print_string uri;
       if Base.String.Search_pattern.matches csv_pattern uri
       then respond_file ~content_type:"text/csv" (Base.String.drop_prefix uri 1)
+      else if Base.String.Search_pattern.matches json_pattern uri
+      then respond_file ~content_type:"application/json" (Base.String.drop_prefix uri 1)
       else respond_string ~content_type:"text/html" ~status:`Not_found ~body:"" ()
   in
   Server.create ~mode:(`TCP (`Port port)) (Server.make ~callback ())
